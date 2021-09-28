2059479bfda034fd3ae0b2128f713d81285d757d
Tomas Pesek <pesekt@gmail.com>
2021-09-28 22:28:52 +0200

    first commit, hello world, controller

---
+++ src/main/java/swc3/demowebshop/DemoWebshopApplication.java
@@ -0,0 +1,13 @@
+package swc3.demowebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swc3/demowebshop/DemoWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package swc3.demowebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
