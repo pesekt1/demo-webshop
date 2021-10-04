d9c042ba3aec42cf0aa3aaecd3b5b978ad506468
Tomas Pesek <pesekt@gmail.com>
2021-10-04 18:57:54 +0200

    Added CORS - WebConfiguration

---
+++ src/main/java/swc3/demowebshop/config/WebConfig.java
@@ -0,0 +1,28 @@
+package swc3.demowebshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:8081",
+                        "http://localhost:8080",
+                        "https://swc3-react-frontend.herokuapp.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true)
+                .maxAge(3600); //1 hour
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
