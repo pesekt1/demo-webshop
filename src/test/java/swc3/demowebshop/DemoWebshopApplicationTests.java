32046f1f84c05d86a122c5d513dbff2ef92fba92
Tomas Pesek <pesekt@gmail.com>
2023-11-20 14:52:34 +0100

    removed tests

--- src/test/java/swc3/demowebshop/DemoWebshopApplicationTests.java
+++ src/test/java/swc3/demowebshop/DemoWebshopApplicationTests.java
@@ -3,11 +3,11 @@ package swc3.demowebshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoWebshopApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class DemoWebshopApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
